17be0b4bc96163e61d08531e7205483685ec8ba6
Johan Larsson <jolars@posteo.com>
2025-06-13 08:59:51 +0200

    chore: update flake lock file

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
-  description = "Development environment for jolars.co website";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.systems.url = "github:nix-systems/default";
   inputs.flake-utils = {
     url = "github:numtide/flake-utils";
@@ -8,17 +8,67 @@
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            bashInteractive
-            quarto
+
+        sortedl1 = (
+          pkgs.python3.pkgs.buildPythonPackage rec {
+            pname = "sortedl1";
+            version = "1.1.0";
+            pyproject = true;
+
+            src = pkgs.fetchPypi {
+              inherit pname version;
+              hash = "sha256-bon1d6r18eayuqhhK8zAckFWGSilX3eUc213HSeO2dQ=";
+            };
+
+            dontUseCmakeConfigure = true;
+
+            build-system = [
+              pkgs.python3.pkgs.scikit-build-core
+              pkgs.python3.pkgs.pybind11
+              pkgs.cmake
+              pkgs.ninja
+            ];
+
+            dependencies = with pkgs.python3.pkgs; [
+              numpy
+              scikit-learn
+              scipy
+              furo
+              sphinx-copybutton
+              myst-parser
+              pytest
+            ];
+
+            disabledTests = [
+              "test_cdist"
+            ];
+
+            pythonImportsCheck = [
+              "sortedl1"
+            ];
+          }
+        );
+
+        buildEnv = pkgs.buildFHSEnv {
+          name = "build-env";
+          targetPkgs = pkgs: [
+            pkgs.bashInteractive
+            pkgs.gcc-unwrapped
+            pkgs.binutils-unwrapped
+            pkgs.quartoMinimal
+            pkgs.glibc
+            pkgs.cmake
+            pkgs.pkg-config
+            pkgs.julia-bin
             (pkgs.rWrapper.override {
               packages = with pkgs.rPackages; [
                 SLOPE
@@ -26,11 +76,25 @@
                 maps
                 qualpalr
                 rgl
+                reticulate
                 venneuler
+                languageserver
+                JuliaCall
               ];
             })
+            (pkgs.python3.withPackages (ps: [
+              ps.matplotlib
+              ps.numpy
+              ps.pandas
+              ps.jupyter
+              ps.openai
+              sortedl1
+            ]))
           ];
         };
+      in
+      {
+        devShells.default = buildEnv.env;
       }
     );
 }
